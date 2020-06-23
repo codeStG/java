95e643dad8e8b013065f79232ec67cba09989f04
George Torres <torresgeorge2014@gmail.com>
2020-06-23 00:08:28 -0500

    Completing Java exercise 2.

--- exercise2/Colors.java
+++ exercise2/Colors.java
@@ -1,17 +1,49 @@
 package exercise2;
 
+import java.util.Arrays;
+
 public class Colors {
 
     String[] colorful(String[] strings) {
-        // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.) return an array of strings that start with a color.  discard all the other strings.  If no strings start with a color in the input array, return an empty array.
-        return new String[]{};
+        // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.)
+        // return an array of strings that start with a color.  discard all the other strings.  If no strings start with
+        // a color in the input array, return an empty array.
+        String[] newStrings = new String[strings.length];
+        int newIndex = 0;
+
+        for(int i = 0; i < strings.length; i++) {
+            String color = strings[i].toLowerCase();
+            if(color.toLowerCase().startsWith("red")) {
+                newStrings[newIndex] = "red";
+                newIndex++;
+            } else if(color.toLowerCase().startsWith("blue")) {
+                newStrings[newIndex] = "blue";
+                newIndex++;
+            } else if(color.toLowerCase().startsWith("yellow")) {
+                newStrings[newIndex] = "yellow";
+                newIndex++;
+            } else if(color.toLowerCase().startsWith("black")) {
+                newStrings[newIndex] = "black";
+                newIndex++;
+            } else if(color.toLowerCase().startsWith("green")) {
+                newStrings[newIndex] = "green";
+                newIndex++;
+            } else if(color.toLowerCase().startsWith("pink")) {
+                newStrings[newIndex] = "pink";
+                newIndex++;
+            } else if ((i == strings.length - 1) && (newIndex == 0)) {
+                System.out.println("No colors found.");
+                return new String[]{};
+            }
+        }
+        return newStrings;
     }
 
     public static void main(String[] args) {
         Colors code = new Colors();
-        code.colorful(new String[]{"bluebird","red herring", "yellow bumblebee","black spider", "green monkey","pink","great gatsby","feeding frenzy","black bird","widowmaker","quran","bible","torah"});
         System.out.println("This is the Colors class");
+        System.out.println(Arrays.toString(code.colorful(new String[]{"bluebird","red herring", "yellow bumblebee","black spider", "green monkey","pink",
+                "great gatsby","feeding frenzy","black bird","widowmaker","quran","bible","torah"})));
     }
-
 
 }
--- exercise2/Statically.java
+++ exercise2/Statically.java
@@ -2,4 +2,39 @@ package exercise2;
 
 public class Statically {
     //Once you have finished getting statistically.js to run, refactor the code here in Java.
+    private static String glue = "Epoxy";
+    String jello = "Jello";
+
+    static void moveAndShake() {
+        System.out.println("Never stagnate in life.  Be agile, change, and improve...");
+    }
+
+    static void stubborn() {
+        moveAndShake();
+        System.out.println("Please don't make me change... I just don't like it...");
+    }
+
+    static void explain() {
+        stubborn();
+        System.out.println(glue);
+        //write an explanation for how the keyword static behaves differently in javascript vs. java.
+        System.out.println("Static in js makes methods within a class usable only within that class, while in Java "
+                + "the static keyword is a non-access modifier.");
+        System.out.println("For the Java implementation of this class, I did the same thing I did in the js representation "
+        + "but changed the private glue variable to be static so that I could access it from within the explain method." +
+                " I also did not have to add any of the call functions that I had to add in the js representation because " +
+                "I am able to call the static methods in the stats instance of the Statically class.");
+    }
+
+
+    public static void main(String[] args) {
+        Statically stats = new Statically();
+
+        stats.moveAndShake();
+        stats.stubborn();
+        stats.explain();
+
+        System.out.println(stats.glue);
+        System.out.println(stats.jello);
+    }
 }
